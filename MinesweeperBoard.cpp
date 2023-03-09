1b97f6028ca5f7ae5c11ba000f2993c88c5c5a90
grzegorz.stasiak_275483 <275483@student.pwr.edu.pl>
2023-03-09 16:07:26 +0100

    comments addition

--- MinesweeperBoard.cpp
+++ MinesweeperBoard.cpp
@@ -3,6 +3,16 @@
 
 using std::cout;
 
+// Konstruktor ustawiający wysokość, szerokość,
+// ilość bomb. Inicjalizuje pole oraz wywołuje
+// funkcje testową
+//
+// Parametry: h - wysokość
+// w - szerokość
+// bombs - ilość bomb
+//
+// Zwraca: -
+//
 MinesweeperBoard::MinesweeperBoard(int h, int w, int bombs)
 {
     height = h;
@@ -13,6 +23,14 @@ MinesweeperBoard::MinesweeperBoard(int h, int w, int bombs)
     test_3_fields();
 }
 
+// Funkcja inicjalizuje plansze o rozmiarach
+// height*width ustawiajac pola na bez bomb,
+// bez flag i zakryte.
+//
+// Parametry: -
+//
+// Zwraca: -
+//
 void MinesweeperBoard::initialize_board(){
     for (int i = 0; i < height; i++)
     {
@@ -25,6 +43,13 @@ void MinesweeperBoard::initialize_board(){
     }
 }
 
+// Funkcja ustawia na 3 polach flagi,
+// bomby i odkrycia w celach testowych.
+//
+// Parametry: -
+//
+// Zwraca: -
+//
 void MinesweeperBoard::test_3_fields(){
     board[0][0].hasMine = true;
     board[1][1].isRevealed = true;
@@ -32,6 +57,14 @@ void MinesweeperBoard::test_3_fields(){
     board[0][2].hasMine = true;
 }
 
+// Funkcja wyswietla w terminalu plansze
+// sapera, a na kazdym polu czy znajduje sie mina,
+// czy pole jest odkryte oraz czy jest oflagowane
+//
+// Parametry: -
+//
+// Zwraca: -
+//
 void MinesweeperBoard::debug_display() const{
     for (int i = 0; i < height; i++)
     {
